e49928bb9f925073bd70c03c92ae037cbbd349eb
Greg Bell <greg@bitwombat.com.au>
2021-05-04 16:01:13 +1000

    Big refactor

--- spiral.py
+++ spiral.py
@@ -1,88 +1,74 @@
 #!/usr/bin/env python
 
 from math import ceil, sqrt
+from collections import namedtuple
 
-coord_transforms = [
-    lambda dist: (-1 * dist, 0),
-    lambda dist: (0, 1 * dist),
-    lambda dist: (1 * dist, 0),
-    lambda dist: (0, -1 * dist),
+Point = namedtuple("Point", "x y")
+
+point_transforms = [
+    lambda dist: Point(-1 * dist, 0),
+    lambda dist: Point(0, 1 * dist),
+    lambda dist: Point(1 * dist, 0),
+    lambda dist: Point(0, -1 * dist),
 ]
 
 
-def which_square(j):
+def get_square(j):
     return ceil(sqrt(j) / 2)
 
 
-def which_anchor(j):
-    square = which_square(j)
+def get_anchor(j):
+    square = get_square(j)
     return ((square - 1) * 2) ** 2 + 1
 
 
-def get_anchor_coord(square):
-    return ((square - 1), -(square - 1))
+def get_side_length(j):
+    square = get_square(j)
+    return square * 2 - 1
 
 
-def which_side(j):
-    square = which_square(j)
-    anchor = which_anchor(j)
-    normalised_j = j - anchor
-    side_length = square * 2 - 1
-    return normalised_j // side_length
+def get_normalised_j(j):
+    anchor = get_anchor(j)
+    return j - anchor
 
 
-def which_slot(j):
-    square = which_square(j)
-    anchor = which_anchor(j)
-    side_length = square * 2 - 1
-    normalised_j = j - anchor
-    return normalised_j % side_length
+def get_side(j):
+    return get_normalised_j(j) // get_side_length(j)
 
 
-def get_side_anchor(square, side):
-    end_to_next_anchor = [(0, 1), (1, 0), (0, -1)]
+def get_position_on_side(j):
+    return get_normalised_j(j) % get_side_length(j)
+
+
+def get_square_anchor_point(square):
+    return Point((square - 1), -(square - 1))
+
+
+def get_side_anchor_point(square, side):
+    translation_to_next_anchor = [Point(0, 1), Point(1, 0), Point(0, -1)]
+    side_anchor = []
+    side_anchor.append(get_square_anchor_point(square))
     side_length = square * 2 - 2  # explain the -2
-    side_anchor = [(0, 0), (0, 0), (0, 0), (0, 0)]
-    side_anchor[0] = get_anchor_coord(square)
 
-    side_end = (
-        side_anchor[0][0] + coord_transforms[0](side_length)[0],
-        side_anchor[0][1] + coord_transforms[0](side_length)[1],
-    )
+    for i in range(0, 3):
+        side_end = Point(
+            side_anchor[i].x + point_transforms[i](side_length).x,
+            side_anchor[i].y + point_transforms[i](side_length).y,
+        )
 
-    side_anchor[1] = (
-        side_end[0] + end_to_next_anchor[0][0],
-        side_end[1] + end_to_next_anchor[0][1],
-    )
-
-    side_end = (
-        side_anchor[1][0] + coord_transforms[1](side_length)[0],
-        side_anchor[1][1] + coord_transforms[1](side_length)[1],
-    )
-    side_anchor[2] = (
-        side_end[0] + end_to_next_anchor[1][0],
-        side_end[1] + end_to_next_anchor[1][1],
-    )
-    side_end = (
-        side_anchor[2][0] + coord_transforms[2](side_length)[0],
-        side_anchor[2][1] + coord_transforms[2](side_length)[1],
-    )
-    side_anchor[3] = (
-        side_end[0] + end_to_next_anchor[2][0],
-        side_end[1] + end_to_next_anchor[2][1],
-    )
+        side_anchor.append(
+            Point(
+                side_end.x + translation_to_next_anchor[i].x,
+                side_end.y + translation_to_next_anchor[i].y,
+            )
+        )
 
     return side_anchor[side]
 
 
 def calc(i):
-    coord = get_side_anchor(which_square(i), which_side(i))
-    offset = which_slot(i)
-    side_length = which_square(i) * 2 - 2  # explain the -2
-    travel = coord_transforms[which_side(i)](offset)
-    our_coord = (coord[0] + travel[0], coord[1] + travel[1])
-    return our_coord
-
-
-# if __name__ == "__main__":
-#    calc(5)
+    side_anchor_point = get_side_anchor_point(get_square(i), get_side(i))
+    position = get_position_on_side(i)
+    translation = point_transforms[get_side(i)](position)
+    point = (side_anchor_point.x + translation.x, side_anchor_point.y + translation.y)
+    return point
--- tests/test_components.py
+++ tests/test_components.py
@@ -1,86 +1,93 @@
 #!/usr/bin/env python
 
-from spiral import which_square, which_anchor, which_side, which_slot, get_side_anchor, get_anchor_coord
+from spiral import (
+    get_square,
+    get_anchor,
+    get_side,
+    get_position_on_side,
+    get_side_anchor_point,
+    get_square_anchor_point,
+)
 
 
-def test_which_square():
-    assert which_square(1) == 1
-    assert which_square(2) == 1
-    assert which_square(3) == 1
-    assert which_square(4) == 1
-    assert which_square(5) == 2
-    assert which_square(13) == 2
-    assert which_square(16) == 2
-    assert which_square(17) == 3
-    assert which_square(35) == 3
-    assert which_square(36) == 3
-    assert which_square(37) == 4
+def test_get_square():
+    assert get_square(1) == 1
+    assert get_square(2) == 1
+    assert get_square(3) == 1
+    assert get_square(4) == 1
+    assert get_square(5) == 2
+    assert get_square(13) == 2
+    assert get_square(16) == 2
+    assert get_square(17) == 3
+    assert get_square(35) == 3
+    assert get_square(36) == 3
+    assert get_square(37) == 4
 
 
 def test_get_anchor():
-    assert which_anchor(1) == 1
-    assert which_anchor(4) == 1
-    assert which_anchor(5) == 5
-    assert which_anchor(13) == 5
-    assert which_anchor(16) == 5
-    assert which_anchor(17) == 17
-    assert which_anchor(36) == 17
-    assert which_anchor(37) == 37
+    assert get_anchor(1) == 1
+    assert get_anchor(4) == 1
+    assert get_anchor(5) == 5
+    assert get_anchor(13) == 5
+    assert get_anchor(16) == 5
+    assert get_anchor(17) == 17
+    assert get_anchor(36) == 17
+    assert get_anchor(37) == 37
 
 
-def test_which_side():
-    assert which_side(1) == 0
-    assert which_side(2) == 1
-    assert which_side(3) == 2
-    assert which_side(4) == 3
-    assert which_side(5) == 0
-    assert which_side(6) == 0
-    assert which_side(7) == 0
-    assert which_side(8) == 1
-    assert which_side(9) == 1
-    assert which_side(10) == 1
-    assert which_side(11) == 2
-    assert which_side(14) == 3
-    assert which_side(16) == 3
+def test_get_side():
+    assert get_side(1) == 0
+    assert get_side(2) == 1
+    assert get_side(3) == 2
+    assert get_side(4) == 3
+    assert get_side(5) == 0
+    assert get_side(6) == 0
+    assert get_side(7) == 0
+    assert get_side(8) == 1
+    assert get_side(9) == 1
+    assert get_side(10) == 1
+    assert get_side(11) == 2
+    assert get_side(14) == 3
+    assert get_side(16) == 3
 
-    assert which_side(17) == 0
-    assert which_side(21) == 0
-    assert which_side(22) == 1
-    assert which_side(28) == 2
-    assert which_side(32) == 3
-    assert which_side(36) == 3
-
-
-def test_which_slot():
-    assert which_slot(1) == 0
-    assert which_slot(2) == 0
-    assert which_slot(3) == 0
-    assert which_slot(4) == 0
-    assert which_slot(5) == 0
-    assert which_slot(6) == 1
-    assert which_slot(7) == 2
-    assert which_slot(8) == 0
+    assert get_side(17) == 0
+    assert get_side(21) == 0
+    assert get_side(22) == 1
+    assert get_side(28) == 2
+    assert get_side(32) == 3
+    assert get_side(36) == 3
 
 
-def test_get_side_anchor():
-    assert get_side_anchor(1, 0) == (0, 0)
-    assert get_side_anchor(1, 1) == (0, 1)
-    assert get_side_anchor(1, 2) == (1, 1)
-    assert get_side_anchor(1, 3) == (1, 0)
-
-    assert get_side_anchor(2, 0) == (1, -1)
-    assert get_side_anchor(2, 1) == (-1, 0)
-    assert get_side_anchor(2, 2) == (0, 2)
-    assert get_side_anchor(2, 3) == (2, 1)
-
-    assert get_side_anchor(3, 0) == (2, -2)
-    assert get_side_anchor(3, 1) == (-2, -1)
-    assert get_side_anchor(3, 2) == (-1, 3)
-    assert get_side_anchor(3, 3) == (3, 2)
+def test_get_position_on_side():
+    assert get_position_on_side(1) == 0
+    assert get_position_on_side(2) == 0
+    assert get_position_on_side(3) == 0
+    assert get_position_on_side(4) == 0
+    assert get_position_on_side(5) == 0
+    assert get_position_on_side(6) == 1
+    assert get_position_on_side(7) == 2
+    assert get_position_on_side(8) == 0
 
 
-def test_get_anchor_coord():
-    assert get_anchor_coord(1) == (0, 0)
-    assert get_anchor_coord(2) == (1, -1)
-    assert get_anchor_coord(3) == (2, -2)
-    assert get_anchor_coord(4) == (3, -3)
+def test_get_side_anchor_point():
+    assert get_side_anchor_point(1, 0) == (0, 0)
+    assert get_side_anchor_point(1, 1) == (0, 1)
+    assert get_side_anchor_point(1, 2) == (1, 1)
+    assert get_side_anchor_point(1, 3) == (1, 0)
+
+    assert get_side_anchor_point(2, 0) == (1, -1)
+    assert get_side_anchor_point(2, 1) == (-1, 0)
+    assert get_side_anchor_point(2, 2) == (0, 2)
+    assert get_side_anchor_point(2, 3) == (2, 1)
+
+    assert get_side_anchor_point(3, 0) == (2, -2)
+    assert get_side_anchor_point(3, 1) == (-2, -1)
+    assert get_side_anchor_point(3, 2) == (-1, 3)
+    assert get_side_anchor_point(3, 3) == (3, 2)
+
+
+def test_get_square_anchor_point():
+    assert get_square_anchor_point(1) == (0, 0)
+    assert get_square_anchor_point(2) == (1, -1)
+    assert get_square_anchor_point(3) == (2, -2)
+    assert get_square_anchor_point(4) == (3, -3)
